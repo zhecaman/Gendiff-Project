3d30b12bf5881a4144bc20e33f8f542f4bc20487
zcmn <zhecaman@icloud.com>
2023-07-27 13:21:39 +0300

    6 step pt2

--- gendiff/gendiff_module.py
+++ gendiff/gendiff_module.py
@@ -1,7 +1,7 @@
 from .parser import parse
 
 
-def generate_diff(filepath1, filepath2):
+def generate_diff(f1, f2):
     """Generate difference between two files
 
     Args:
@@ -9,43 +9,44 @@ def generate_diff(filepath1, filepath2):
         filepath2 (str): path to file 2
     """
 
-    def if_bool(value):
-        return "true" if value else "false"
-
-    output = "{\n"
-
-    def make_diff(f1, f2):
-        """takes two objects and returns list of formatted strings based on
-        files key-value  difference
-        """
-        result = []
-        
-
-        for key1 in f1.keys():
-            if isinstance(f1[key1], bool):
-                f1[key1] = if_bool(f1[key1])
-
-            if key1 in f2.keys() and f1[key1] == f2[key1]:
-                formatted = f"  {key1}: {f1[key1]}"
-                result.append(formatted)
-            elif key1 not in f2.keys():
-                formatted = f"- {key1}: {f1[key1]}"
-                result.append(formatted)
-            elif key1 in f2.keys() and f1[key1] != f2[key1]:
-                formatted = f"- {key1}: {f1[key1]}"
-                formatted2 = f"+ {key1}: {f2[key1]}"
-                result.append(formatted)
-                result.append(formatted2)
-        for key2 in f2.keys():
-            if isinstance(f2[key2], bool):
-                f2[key2] = if_bool(f2[key2])
-            if key2 not in f1.keys():
-                formatted = f"+ {key2}: {f2[key2]}"
-                result.append(formatted)
-        return sorted(result, key=lambda x: x[2])
-
-    data1 = parse(filepath1)
-    data2 = parse(filepath2)
-    formatted_data = make_diff(data1, data2)
-    output += "\n".join(formatted_data)
-    return output + "\n}"
+    keys = sorted(f1.keys() | f2.keys())
+    result = dict()
+    
+    for key in keys:
+        if key not in f2:
+            result[key] = {
+                'type' : 'deleted',
+                'value' : f1[key],
+                'children' : None,
+            }
+            
+        elif key not in f1:
+            result[key] = {
+                'type' : 'added',
+                'value' : f2[key],
+                'children' : None,
+            }
+            
+        elif f1[key] == f2[key]:
+            result[key] = {
+                'type' : 'unchanged',
+                'value' : f1[key],
+                'children' : None,
+            }
+                
+        elif isinstance(f1[key], dict) and isinstance(f2[key], dict):
+            result[key] = {
+                    'type' : 'dict',
+                    'value' : None,
+                    'children' : generate_diff(f1[key], f2[key]),
+                }
+        else: 
+            result[key] = {
+                    'type' : 'changed',
+                    'value' : {
+                        'old' : f1[key],
+                        'new' : f2[key],
+                    },
+                    'children' : None,
+                }
+    return result
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,11 +1,16 @@
 from gendiff import generate_diff
-from tests.fixtures.right import RIGHT
+from tests.fixtures.right import RIGHT_PLAIN, RIGHT_NESTED
 
 
 def test_plain():
-    assert RIGHT == generate_diff(
+    assert RIGHT_PLAIN == generate_diff(
         "tests/fixtures/file1.json", "tests/fixtures/file2.json"
     )
-    assert RIGHT == generate_diff(
+    assert RIGHT_PLAIN == generate_diff(
         "tests/fixtures/file1.yaml", "tests/fixtures/file2.yaml"
+    )
+
+def test_nested():
+    assert RIGHT_NESTED == generate_diff(
+        "tests/fixtures/file3.json", "tests/fixtures/file4.json"
     )
