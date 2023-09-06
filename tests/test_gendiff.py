6508f65c88bc4769476a9c22a9878cced17c6ada
zcmn <zhecaman@icloud.com>
2023-09-06 14:48:58 +0300

    edited tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,41 +2,39 @@ import pytest
 from gendiff.gendiff import generate_diff
 
 
-SIMPLE1_JSON = "tests/fixtures/simple1.json"
-SIMPLE2_JSON = "tests/fixtures/simple2.json"
-SIMPLE1_YAML = "tests/fixtures/simple1.yaml"
-SIMPLE2_YAML = "tests/fixtures/simple2.yaml"
-NESTED1_JSON = "tests/fixtures/nested1.json"
-NESTED2_JSON = "tests/fixtures/nested2.json"
-NESTED1_YAML = "tests/fixtures/nested1.yaml"
-NESTED2_YAML = "tests/fixtures/nested2.yaml"
-ANSWER_JSON_SIMPLE = "tests/fixtures/answer_simple_json"
-ANSWER_PLAIN_SIMPLE = "tests/fixtures/answer_simple_plain"
-ANSWER_STYLISH_SIMPLE = "tests/fixtures/answer_simple_stylish"
-ANSWER_JSON_NESTED = "tests/fixtures/answer_nested_json"
-ANSWER_PLAIN_NESTED = "tests/fixtures/answer_nested_plain"
-ANSWER_STYLISH_NESTED = "tests/fixtures/answer_nested_stylish"
+NESTED1_JSON = "nested1.json"
+NESTED2_JSON = "nested2.json"
+NESTED1_YAML = "nested1.yaml"
+NESTED2_YAML = "nested2.yaml"
+ANSWER_JSON_NESTED = "answer_nested_json"
+ANSWER_PLAIN_NESTED = "answer_nested_plain"
+ANSWER_STYLISH_NESTED = "answer_nested_stylish"
 
 
-def read_answer(filepath):
-    with open(filepath) as file:
+def get_path_to_fixture(filename):
+    return f'tests/fixtures/{filename}'
+
+
+def read_answer(filename):
+    with open(get_path_to_fixture(filename)) as file:
         return file.read()
 
 
 @pytest.mark.parametrize(
     ("filepath1, filepath2, formatter, expected"),
     [
-        (SIMPLE1_JSON, SIMPLE2_JSON, "json", ANSWER_JSON_SIMPLE),
-        (SIMPLE1_YAML, SIMPLE2_YAML, "json", ANSWER_JSON_SIMPLE),
-        (SIMPLE1_JSON, SIMPLE2_JSON, "stylish", ANSWER_STYLISH_SIMPLE),
-        (SIMPLE1_JSON, SIMPLE2_JSON, "plain", ANSWER_PLAIN_SIMPLE),
-        (SIMPLE1_YAML, SIMPLE2_YAML, "stylish", ANSWER_STYLISH_SIMPLE),
-        (NESTED1_YAML, NESTED2_YAML, "plain", ANSWER_PLAIN_NESTED),
-        (NESTED1_YAML, NESTED2_YAML, "stylish", ANSWER_STYLISH_NESTED),
-        (NESTED1_YAML, NESTED2_YAML, "json", ANSWER_JSON_NESTED),
-        (NESTED1_JSON, NESTED2_JSON, "json", ANSWER_JSON_NESTED),
-        (NESTED1_JSON, NESTED2_JSON, "stylish", ANSWER_STYLISH_NESTED),
-        (NESTED1_JSON, NESTED2_JSON, "plain", ANSWER_PLAIN_NESTED),
+        (get_path_to_fixture(NESTED1_YAML), get_path_to_fixture(
+            NESTED2_YAML), "plain", ANSWER_PLAIN_NESTED),
+        (get_path_to_fixture(NESTED1_YAML), get_path_to_fixture(
+            NESTED2_YAML), "stylish", ANSWER_STYLISH_NESTED),
+        (get_path_to_fixture(NESTED1_YAML), get_path_to_fixture(
+            NESTED2_YAML), "json", ANSWER_JSON_NESTED),
+        (get_path_to_fixture(NESTED1_JSON), get_path_to_fixture(
+            NESTED2_JSON), "json", ANSWER_JSON_NESTED),
+        (get_path_to_fixture(NESTED1_JSON), get_path_to_fixture(
+            NESTED2_JSON), "stylish", ANSWER_STYLISH_NESTED),
+        (get_path_to_fixture(NESTED1_JSON), get_path_to_fixture(
+            NESTED2_JSON), "plain", ANSWER_PLAIN_NESTED),
     ],
 )
 def test_gendiff(filepath1, filepath2, formatter, expected):
